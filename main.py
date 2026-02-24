3f21ffb04bf6972ed1a7c71b67e75186365be43c
Yael <beltrancolosio@gmail.com>
2026-02-23 22:03:53 -0800

    Added main and script structure

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+import script
+import os
+
 def main():
     print("Starting Stock Prices Analysis...")
+    excel_file = "netflix.csv"
+    column_name = "Closee"
+
+    # Read the Excel file
+    df = script.read_file(excel_file)
+
+    # Check Column
+    if script.check_column(df, column_name):
+        column_data = df[column_name]
+    else:
+        print("Column validation failed. Exiting.")
+        return
+
+
 
 if __name__ == '__main__':
+    os.chdir('Stock-Prices-Analysis') # Change to your subfolder
     main()
--- script.py
+++ script.py
@@ -1,10 +1,9 @@
 
-import os
 import pandas as pd
 from pathlib import Path
 
-
-def read_file(file_path, column_name):
+# Read CSV or XLSX file and return df
+def read_file(file_path):
     """
     Read CSV or XLSX and validate column.
     
@@ -20,43 +19,45 @@ def read_file(file_path, column_name):
         file_ext = Path(file_path).suffix.lower()
         
         if file_ext == '.csv':
-            df = pd.read_csv(file_path)
             print(f"CSV file loaded")
+            return pd.read_csv(file_path)
+      
         elif file_ext in ['.xlsx', '.xls']:
-            df = pd.read_excel(file_path)
             print(f"Excel file loaded")
+            return pd.read_excel(file_path)
         else:
             print(f"Unsupported file type: {file_ext}")
-            return False
-        
-        if column_name in df.columns:
-            column_data = df[column_name]
-            # Check if column is fully numeric
-            if pd.api.types.is_numeric_dtype(column_data):
-                print(f"Column '{column_name}' is fully numeric")
-                return True
-            else:
-                print(f"Column '{column_name}' is not numeric")
-                return False
-        else:
-            print(f"Column '{column_name}' not found")
-            print(f"Available columns: {df.columns.tolist()}")
-            return False
+            return None
     
     except FileNotFoundError:
         print(f"File not found: {file_path}")
-        return False
+        return None
     except Exception as e:
         print(f"Error reading file: {e}")
+        return None
+
+def check_column(df, column_name):
+    if column_name in df.columns:
+        column_data = df[column_name]
+        # Check if column is fully numeric
+        if pd.api.types.is_numeric_dtype(column_data):
+            print(f"Column '{column_name}' is fully numeric")
+            return True
+        else:
+            print(f"Column '{column_name}' is not numeric")
+            return False
+    else:
+        print(f"Column '{column_name}' not found")
+        print(f"Available columns: {df.columns.tolist()}")
         return False
 
 
 # Read the Excel file
-os.chdir('Stock-Prices-Analysis') # Change to your subfolder
-excel_file = "netflix.csv"
-column_name = "Close"
+#os.chdir('Stock-Prices-Analysis') # Change to your subfolder
+#excel_file = "netflix.csv"
+#column_name = "Close"
 
 # Get Column data
-column_data = read_file(excel_file, column_name)
-print(column_data)
+#column_data = read_file(excel_file, column_name)
+#print(column_data)
 
